047d0d899a6e9540f18bdf3413fe4a66b9095327
Joes131205 <Joes131205@users.noreply.github.com>
2023-12-31 22:47:08 +0700

    hope this is final

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const body = document.getElementsByTagName("html")[0];
+import songs from "./songs.js";
+const song = document.getElementById("song");
 
 // Countdown elements
 const dayEl = document.getElementById("day");
@@ -11,8 +13,27 @@ const nextYearEl = document.getElementById("next-year");
 
 // Music element
 const playingEl = document.getElementById("playing");
+
+// API Link
 const url =
     "https://api.unsplash.com/search/photos/?client_id=uZRS5NNoUW_VgyyGyklNRvqNZ3AU7lop1KE1pq2x6l8&query=winter%20background&per_page=20&orientation=landscape";
+
+// Handle Songs
+function switchSong() {
+    const randomAudio = songs[Math.floor(Math.random() * songs.length)];
+    song.src = randomAudio.path;
+    song.play();
+    playingEl.textContent = `${randomAudio.title} - ${randomAudio.author}`;
+}
+
+song.addEventListener("ended", function () {
+    switchSong();
+});
+
+switchSong();
+song.play();
+
+// Handle background changes
 
 async function setImage() {
     const response = await fetch(url);
@@ -23,18 +44,28 @@ async function setImage() {
             .regular;
     return imageUrl;
 }
+
 setInterval(() => {
     setImage().then((url) => {
         body.style.background = `url(${url}) center / 100% no-repeat`;
     });
-}, 100000000000);
+}, 10000);
+
+// Handle countdown
 
 function setCountdown() {
     setInterval(() => {
         const nextYearDate = new Date("1 January 2024");
         const currentDate = new Date();
         const timeDifference = nextYearDate.getTime() - currentDate.getTime();
-
+        if (timeDifference < 0) {
+            dayEl.textContent = "HAP";
+            hourEl.textContent = "PY";
+            minuteEl.textContent = "NEW";
+            secondEl.textContent = "YEAR!";
+            nextYearEl.textContent = "🎉2024🎉";
+            return;
+        }
         const daysLeft = Math.floor(timeDifference / (1000 * 24 * 60 * 60));
         const hoursLeft = Math.floor(
             (timeDifference % (1000 * 24 * 60 * 60)) / (1000 * 60 * 60)
---
+++ songs.js
@@ -0,0 +1,53 @@
+const songs = [
+    {
+        title: "Fresh Beginning",
+        author: "AudioVision",
+        path: "./assets/song/audiovision-fresh-beginning-182343.mp3",
+    },
+    {
+        title: "Vintage Sleigh",
+        author: "AudioVision",
+        path: "./assets/song/audiovision-vintage-sleigh-182341.mp3",
+    },
+    {
+        title: "Enchanted Chimes",
+        author: "Top-Flow-Production",
+        path: "./assets/song/enchanted-chimes-177906.mp3",
+    },
+    {
+        title: "Good Night",
+        author: "FASSounds",
+        path: "./assets/song/good-night-160166.mp3",
+    },
+    {
+        title: "Fluid",
+        author: "ItsWatR",
+        path: "./assets/song/watr-fluid-10149.mp3",
+    },
+    {
+        title: "Tokyo Cafe",
+        author: "TVARI",
+        path: "./assets/song/tvari-tokyo-cafe-159065.mp3",
+    },
+    {
+        title: "Futuristic Beat",
+        author: "RoyaltyFreeMusic",
+        path: "./assets/song/futuristic-beat-146661.mp3",
+    },
+    {
+        title: "Christmas Eve",
+        author: "SoulProdMusic",
+        path: "./assets/song/futuristic-beat-146661.mp3",
+    },
+    {
+        title: "Snow",
+        author: "SoulProdMusic",
+        path: "./assets/song/snow-180943.mp3",
+    },
+    {
+        title: "Christmas Tree Lofi",
+        author: "SoulProdMusic",
+        path: "./assets/song/christmas-tree-chillhop-lofi-christmas-background-music-126617.mp3",
+    },
+];
+export default songs;
